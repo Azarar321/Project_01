ffde53cc1fbe8fa89bfd1a74490afd3ea4ec7eef
Azarar321 <azarar321@gmail.com>
2020-10-24 14:56:51 +0530

    king

--- src/test/java/org/samp/Sample.java
+++ src/test/java/org/samp/Sample.java
@@ -5,7 +5,16 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		System.out.println("Line 1");
-		
+		System.out.println("donnee");
+		System.out.println("Line 2");
+		System.out.println("Line 1");
+		System.out.println("donnee");
+		System.out.println("Line 2");
+		System.out.println("Line 1");
+		System.out.println("donnee");
+		System.out.println("Line 2");
+		System.out.println("Line 1");
+		System.out.println("donnee");
 		System.out.println("Line 2");
 		
 	}
